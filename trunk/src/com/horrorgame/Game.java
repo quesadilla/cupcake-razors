d4e32d1df13e3b080568dee28131f864a0c03c38
mast.ryan@gmail.com <mast.ryan@gmail.com@4da10241-18bf-04ab-8805-af4b5a736641>
2013-01-09 04:27:47 +0000

    Changed sprite

--- trunk/src/com/horrorgame/Game.java
+++ trunk/src/com/horrorgame/Game.java
@@ -23,6 +23,11 @@ public class Game implements ApplicationListener {
 	public static int SCREEN_WIDTH = 800;
 	public static int SCREEN_HEIGHT = 400;
 	
+	public static final int PLAYER_WALK = 1;
+	public static final int PLAYER_RUN = 3;
+	public static final int MONSTER_WALK = 2;
+	public static final float PLAYER_RUN_DELAY = (float) 2.3;
+	
 	// things to draw
 	SpriteBatch batch;
 	Texture texture;
@@ -31,12 +36,14 @@ public class Game implements ApplicationListener {
 	TextureRegion playerStand;
 	TextureRegion playerPush;
 	List<AtlasRegion> playerWalk;
+	List<AtlasRegion> playerRun;
 	InputProcessor processor;
 	Rectangle player;
 	int currentFrame;
 	int currSpeed;
 	float frameTime;
 	float timeHiding;
+	float totalWalkTime;
 	boolean goingLeft;
 	boolean hiding;
 	BitmapFont font;
@@ -47,19 +54,21 @@ public class Game implements ApplicationListener {
 	@Override
 	public void create() {
 		
-		atlas = new TextureAtlas(Gdx.files.internal("assets/assets_horror.pack"));
+		atlas = new TextureAtlas(Gdx.files.internal("assets/images.pack"));
 		
 		closet_open = atlas.findRegion("open_closet");
 		closet_close = atlas.findRegion("close_closet");
 		playerPush = atlas.findRegion("push");
 		playerStand = atlas.findRegion("stand");
-		playerWalk = atlas.findRegions("walk");
+		playerWalk = atlas.findRegions("mainw");
+		playerRun = atlas.findRegions("mainr");
 		frameTime = 0;
 		currentFrame = 0;
 		goingLeft = false;
 		timeHiding = 0;
 		hiding = false;
-		currSpeed = 1;
+		totalWalkTime = 0;
+		currSpeed = PLAYER_WALK;
 		
 		font = new BitmapFont();
 		font.setColor(0,0,0,1);
@@ -67,8 +76,8 @@ public class Game implements ApplicationListener {
 		batch = new SpriteBatch();
 
 		player = new Rectangle();
-		player.width = 30;
-		player.height = 120;
+		player.width = 100;
+		player.height = 150;
 		player.x = 100;
 		player.y = 100;
 		
@@ -92,7 +101,14 @@ public class Game implements ApplicationListener {
 		{
 			if (!goingLeft) {
 				playerPush.flip(true, false);
-				currSpeed = 1;
+				for (int i = 0; i < playerWalk.size(); i++) {
+					playerWalk.get(i).flip(true,  false);
+				}
+				for (int i = 0; i < playerRun.size(); i++) {
+					playerRun.get(i).flip(true,  false);
+				}
+				currSpeed = PLAYER_WALK;
+				totalWalkTime = 0;
 			}
 			player.x -= currSpeed;
 			goingLeft = true;
@@ -103,7 +119,14 @@ public class Game implements ApplicationListener {
 		{
 			if (goingLeft) {
 				playerPush.flip(true, false);
-				currSpeed = 1;
+				for (int i = 0; i < playerWalk.size(); i++) {
+					playerWalk.get(i).flip(true,  false);
+				}
+				for (int i = 0; i < playerRun.size(); i++) {
+					playerRun.get(i).flip(true,  false);
+				}
+				currSpeed = PLAYER_WALK;
+				totalWalkTime = 0;
 			}
 			player.x += currSpeed;
 			goingLeft = false;
@@ -137,21 +160,28 @@ public class Game implements ApplicationListener {
 			} 
 			else if (!Gdx.input.isKeyPressed(Keys.LEFT) && !Gdx.input.isKeyPressed(Keys.RIGHT)) {
 				currentFrame = 0;
-				batch.draw(playerStand, player.x, player.y, player.width, player.height);
+				batch.draw(playerWalk.get(currentFrame), player.x, player.y, player.width, player.height);
 			} 
 			else {
 				frameTime += Gdx.graphics.getDeltaTime();
-				if (frameTime > .25) {
+				if (frameTime > .1) {
+					totalWalkTime += frameTime;
 					frameTime = 0;
 					currentFrame++;
-					if (currentFrame > 3) {
+					if (totalWalkTime > PLAYER_RUN_DELAY) {
+						currSpeed = PLAYER_RUN;
+					}
+					if (currentFrame >= playerWalk.size()) {
 						currentFrame = 0;
-						if (currSpeed < 3) {
-							currSpeed++;
-						}
+						//if (currSpeed < 3) {
+						//	currSpeed++;
+						//}
 					}
 				}
-				batch.draw(playerWalk.get(currentFrame), player.x, player.y, player.width, player.height);
+				if (currSpeed == PLAYER_WALK)
+					batch.draw(playerWalk.get(currentFrame), player.x, player.y, player.width, player.height);
+				else
+					batch.draw(playerRun.get(currentFrame), player.x, player.y, player.width, player.height);
 			}
 		}
 		batch.end();
