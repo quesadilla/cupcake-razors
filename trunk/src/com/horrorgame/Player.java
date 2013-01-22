98f06e8c76cf0d16a09615461b89f921250a8345
mast.ryan@gmail.com <mast.ryan@gmail.com@4da10241-18bf-04ab-8805-af4b5a736641>
2013-01-22 02:58:55 +0000

    Moved player code into its own class.

---
+++ trunk/src/com/horrorgame/Player.java
@@ -0,0 +1,210 @@
+package com.horrorgame;
+
+import java.util.List;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+
+public class Player {
+	enum STATE {RUN, WALK, PUSH, STAND, HIDE};
+	
+	public static final int PLAYER_WALK = 1; // speed of player walk
+	public static final int PLAYER_RUN = 3; // speed of player run
+	
+	private TextureRegion playerStand;
+	private List<AtlasRegion> playerPush;
+	private List<AtlasRegion> playerWalk;
+	private List<AtlasRegion> playerRun;
+	
+	private boolean facingLeft;
+	private float stamina;
+	private STATE playerState;
+	
+	private int currentFrame;
+	private float frameTime;
+	private float moveTime;
+	
+	private int x, y, height, width;
+	
+	public Player(TextureAtlas atlas) {
+		playerStand = atlas.findRegion("idle");
+		playerPush = atlas.findRegions("push");
+		playerWalk = atlas.findRegions("walk/mainw");
+		playerRun = atlas.findRegions("run/mainr");
+		
+		playerState = STATE.STAND;
+		facingLeft = false;
+		currentFrame = 0;
+		frameTime = 0;
+		moveTime = 0;
+		width = 100;
+		height = 150;
+		x = 100;
+		y = 100;
+		stamina = 100;
+	}
+	
+	public void update(float timeDelta)
+	{
+		frameTime += timeDelta;
+		moveTime += timeDelta;
+		
+		if (frameTime > .1) {
+			frameTime = 0;
+			if (playerState == STATE.WALK) {
+				currentFrame++;
+				if (currentFrame >= playerWalk.size()) {
+					currentFrame = 0;
+				}
+			} else if (playerState == STATE.RUN) {
+				currentFrame++;
+				if (currentFrame >= playerWalk.size()) {
+					currentFrame = 0;
+				}
+			}
+		}
+		
+		if (playerState == STATE.WALK && moveTime > .01) {
+			if (facingLeft) {
+				x -= PLAYER_WALK;
+			} else {
+				x += PLAYER_WALK;
+			}
+		} else if (playerState == STATE.RUN && moveTime > .01) {
+			if (facingLeft) {
+				if (stamina > 0)
+					x -= PLAYER_RUN;
+				else
+					x -= PLAYER_WALK;
+			} else {
+				if (stamina > 0)
+					x += PLAYER_RUN;
+				else
+					x += PLAYER_WALK;
+			}
+			
+			if (stamina > 0) {
+				stamina -= .5;
+			} 
+		}
+		
+		if (playerState != STATE.RUN) {
+			if (stamina < 100) {
+				stamina += 1;
+			} 
+		}
+	}
+	
+	public void goLeft(boolean run) {
+		if (playerState != STATE.HIDE) {
+			if (!facingLeft) {
+				flipImages();
+				facingLeft = true;
+			}
+			
+			if (run) {
+				if (playerState != STATE.RUN) {
+					currentFrame = 0;
+					frameTime = 0;
+					moveTime = 0;
+				}
+				
+				playerState = STATE.RUN;
+			} else {
+				if (playerState != STATE.WALK) {
+					currentFrame = 0;
+					frameTime = 0;
+					moveTime = 0;
+				}
+				playerState = STATE.WALK;
+			}
+		}
+	}
+	
+	public void goRight(boolean run) {
+		if (playerState != STATE.HIDE) {
+			if (facingLeft) {
+				flipImages();
+				facingLeft = false;
+			}
+			
+			if (run) {
+				if (playerState != STATE.RUN) {
+					currentFrame = 0;
+					frameTime = 0;
+					moveTime = 0;
+				}
+				playerState = STATE.RUN;
+			} else {
+				if (playerState != STATE.WALK) {
+					currentFrame = 0;
+					frameTime = 0;
+					moveTime = 0;
+				}
+				playerState = STATE.WALK;
+			}
+		}
+	}
+	
+	public void push() {
+		if (playerState != STATE.HIDE) {
+			playerState = STATE.PUSH;
+		}
+	}
+	
+	public void stand() {
+		playerState = STATE.STAND;
+	}
+	
+	public void hide() {
+		playerState = STATE.HIDE;
+	}
+	
+	private void flipImages() {
+		playerStand.flip(true, false);
+		for (int i = 0; i < playerPush.size(); i++) {
+			playerPush.get(i).flip(true,  false);
+		}
+		for (int i = 0; i < playerWalk.size(); i++) {
+			playerWalk.get(i).flip(true,  false);
+		}
+		for (int i = 0; i < playerRun.size(); i++) {
+			playerRun.get(i).flip(true,  false);
+		}
+	}
+	
+	public void draw(SpriteBatch batch) {
+		switch (playerState) {
+		case WALK:
+			batch.draw(playerWalk.get(currentFrame), x, y, width, height);
+			break;
+		case RUN:
+			batch.draw(playerRun.get(currentFrame), x, y, width, height);
+			break;
+		case STAND:
+			batch.draw(playerStand, x, y, width, height);
+			break;
+		case PUSH:
+			batch.draw(playerPush.get(0), x, y, width, height);
+			break;
+		case HIDE:
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public boolean isHiding() {
+		return playerState == STATE.HIDE;
+	}
+}
